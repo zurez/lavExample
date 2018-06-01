e985bd47e4762b0b1ea1990eff9b50b22411ee4e
Janis Purins <janis@purins.lv>
2018-06-01 14:03:48 +0300

    Init

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Helpers\InfusionsoftHelper;
+use Illuminate\Http\Request;
+use Response;
+
+class ApiController extends Controller
+{
+    // Todo: Module reminder assigner
+
+    private function exampleCustomer(){
+        // Create Infusionsoft customer with "ipa,iea" courses
+
+        // Create User
+
+        // Attach completed modules
+    }
+}
---
+++ app/Http/Controllers/InfusionsoftController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Helpers\InfusionsoftHelper;
+use Request;
+use Storage;
+use Response;
+
+class InfusionsoftController extends Controller
+{
+    public function authorizeInfusionsoft(){
+        return (new InfusionsoftHelper())->authorize();
+    }
+
+    public function testInfusionsoftIntegrationGetEmail($email){
+
+        $infusionsoft = new InfusionsoftHelper();
+
+        return Response::json($infusionsoft->getContact($email));
+    }
+
+    public function testInfusionsoftIntegrationAddTag($contact_id, $tag_id){
+
+        $infusionsoft = new InfusionsoftHelper();
+
+        return Response::json($infusionsoft->addTag($contact_id, $tag_id));
+    }
+
+    public function testInfusionsoftIntegrationGetAllTags(){
+
+        $infusionsoft = new InfusionsoftHelper();
+
+        return Response::json($infusionsoft->getAllTags());
+    }
+}
