84fd057bb7953011bad8739a41251201909891ef
zurez <zurez4u@gmail.com>
2018-06-13 02:08:36 +0530

    tests

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -13,8 +13,44 @@ class ApiTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    private $url="/api/module_reminder_assigner";
+    private $post_data=[
+        "contact_email"=>"5b1eddfa78d56@test.com",
+        "test"=>true
+    ];
+    public function testPass()
     {
-        $this->assertTrue(true);
+
+        $response = $this->json('POST',$this->url,$this->post_data);
+
+        $response->assertStatus(200)
+        ->assertJson([
+                'success' => true,
+        ]);;
     }
+
+    public function testFail()
+    {
+        # code...
+        $response = $this->json('POST',$this->url,["test"=>true]);
+
+        $response->assertStatus(200)
+        ->assertJson([
+                'success' => false,
+        ]);;
+
+    }
+
+    public function testNoUser()
+    {
+        # code...
+         $response = $this->json('POST',$this->url,["test"=>true,
+            "contact_email"=>"123444"]);
+
+        $response->assertStatus(200)
+        ->assertJson([
+                'success' => false,
+        ]);;
+    }
+    
 }
